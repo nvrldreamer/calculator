2120b4707ea80f6e2ad8b379eaabf493ff08ce9a
MajaZa <zadwornamaja87@gmail.com>
2025-04-30 00:11:25 +0200

    dodanie funkcji

--- script.js
+++ script.js
@@ -1,31 +1,63 @@
 var wynik = document.getElementById("wynik");
-var button = document.getElementById("btnEqual");
-var liczba_a, lcizba_b = 0;
+var button = document.getElementById("btnEquals").addEventListener("click", calculate);
+
+var liczba_a, liczba_b = 0;
 var operacja = "";
 
 var btnPlus = document.getElementById("btnPlus").addEventListener("click", () => {
     operacja = "+"
     getValue()
-    console.log(liczba_a)
 });
 
 var btnMinus = document.getElementById("btnMinus").addEventListener("click", () => {
     operacja = "-"
-    liczba_a = wynik.value
+    getValue()
 });
 
 var btnMultiply = document.getElementById("btnMultiply").addEventListener("click", () => {
     operacja = "*"
-    liczba_a = wynik.value
+    getValue()
 });
-var btnDivide = document.getElementById("btnDivide")/addEventListener("click", () => {
+var btnDivide = document.getElementById("btnDivide").addEventListener("click", () => {
     operacja = "/"
-    liczba_a = wynik.value
+    getValue()
 });
 
 function getValue(){
     liczba_a = wynik.value
     if (liczba_a == "" || isNaN(liczba_a)) {
         alert("Liczba jest niepoprawna")
+        wynik.value = ""
+    }
+    else{
+        wynik.place
+        liczba_a = parseInt(liczba_a)
+        wynik.value = ""
+    }
+    
+}
+
+function calculate() { 
+    liczba_b = wynik.value;
+    if (liczba_b === "" || isNaN(liczba_b)) {
+        alert("Liczba jest niepoprawna");
+    } else {
+        liczba_b = parseFloat(liczba_b);
+        switch (operacja) {
+            case "+":
+                wynik.value = (liczba_a + liczba_b).toString();
+                break;
+            case "-":
+                wynik.value = (liczba_a - liczba_b).toString();
+                break;
+            case "*":
+                wynik.value = (liczba_a * liczba_b).toString();
+                break;
+            case "/":
+                wynik.value = (liczba_a / liczba_b).toString();
+                break;
+            default:
+                alert("Nie wybrano operacji");
+        }
     }
 }
